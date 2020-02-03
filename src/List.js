4586c9740a972079c417d9eaaac3a40cb82e2b80
ryuilhwan <ih.ryu.dev@gmail.com>
2020-02-03 11:48:11 +0900

    complate

--- src/List.js
+++ src/List.js
@@ -1,28 +1,73 @@
-const listSize = Symbol('listSize')
-const dataStore = Symbol('dataStore')
-const pos = Symbol('pos')
-
 export class List extends Array {
-  dataStore(){
-    return this[dataStore]
+  #currIndex = 0
+  get currIndex () {
+    return this.#currIndex
   }
-  append(el) {
-    this.push(el)
+
+  front () {
+    this.#currIndex = 0
     return this
   }
 
-  remove(el) {
-    const foundAt = this[dataStore].indexOf(el)
-    if (foundAt> -1) {
-      this[dataStore].splice(foundAt, 1)
-      --this[listSize]
+  end () {
+    this.#currIndex = this.length - 1
+    return this
+  }
+
+  prev () {
+    if (this.#currIndex > 0) this.#currIndex--
+    return this
+  }
+
+  next () {
+    if (this.#currIndex < this.length - 1) this.#currIndex++
+    return this
+  }
+
+  moveTo (idx) {
+    this.#currIndex = idx
+    return this
+  }
+
+  getElement(){
+    return this[this.#currIndex]
+  }
+  append (...el) {
+    this.push(...el)
+    return this
+  }
+
+  remove (el) {
+    const elIndex = typeof el === 'function'
+      ? this.findIndex(el)
+      : this.indexOf(el)
+    if (elIndex !== -1) {
+      this.splice(elIndex, 1)
+      return this
+    } else {
       return this
     }
-    return this
   }
 
-  length() {
-    return this[listSize]
+  insert (el, after) {
+    const afterIndex = typeof after === 'function' ?
+      this.findIndex(after) :
+      this.indexOf(after)
+    if (afterIndex !== -1) {
+      this.splice(afterIndex, 0, el)
+      return this
+    } else {
+      return this
+    }
+  }
+  *[Symbol.iterator](){
+    let memo = this.#currIndex
+    while(this.#currIndex<this.length){
+      yield this[this.#currIndex]
+      this.#currIndex++
+    }
+    this.#currIndex = memo
+
   }
 }
 
--- test/List.test.js
+++ test/List.test.js
@@ -1,9 +1,8 @@
 import { List } from '../src/List'
 import { strict as assert } from 'assert'
-
 describe('List', () => {
   let list
-  before(()=>{
+  beforeEach(()=>{
     list = new List()
   })
   it('pos - Index of Current Position in list')
@@ -11,21 +10,53 @@ describe('List', () => {
   it('clear')
   it('toString')
   it('getElement')
-  it('insert')
+  it('insert', ()=>{
+    list.append(1, 2, 4).insert(3,4)
+    assert.deepEqual(list, new List(1,2,3,4))
+    list.append(5,6,8).insert(7,8)
+    assert.deepEqual(list, new List(1,2,3,4,5,6,7,8))
+  })
   it('append',()=>{
     list.append(1)
     assert.equal(list.length,1)
   })
- /* it('remove', ()=>{
-    assert.deepEqual(list.remove(1).dataStore(), [])
-    list.append(1)
-    list.append(2)
-    assert.deepEqual(list.remove(1).dataStore(), [2])
-  })*/
-  it('front')
-  it('end')
-  it('prev')
-  it('next')
-  it('currPos')
-  it('moveTo')
+  it('remove', ()=>{
+    let res = list.append(1).remove(1)
+    assert.equal(res.toString(),[].toString())
+    res = list.append({a:1,b:2}, {a:2, c:3}).remove(v=>v.c===3)
+    assert.equal(JSON.stringify(res), JSON.stringify([{a:1, b:2}]))
+  })
+  describe('traversing', ()=>{
+    it('front', ()=>{
+      assert.equal(list.front().currIndex, 0)
+    })
+    it('end', ()=>{
+      list.append(1,2,3)
+      assert.equal(list.end().currIndex, 2)
+    })
+    it('next', ()=>{
+      list.append(1,2,3)
+      assert.equal(list.next().currIndex, 1)
+    })
+    it('prev', ()=>{
+      list.append(1,2,3)
+      assert.equal(list.next().prev().currIndex, 0)
+    })
+    it('moveTo', ()=>{
+      list.append(1,2,3)
+      assert.equal(list.moveTo(1).currIndex,1)
+    })
+    it('getElement', ()=>{
+
+      list.append(1,2,3)
+      assert.equal(list.moveTo(1).getElement(), 2)
+    })
+  })
+  it('iter',()=>{
+    list.append(1,2,3)
+    list.next()
+    const elements = [...list]
+    assert.deepEqual(elements,[2,3])
+    assert.deepEqual(list.currIndex, 1)
+  })
 })
