db5b3293b17f9c3aed920e88c06fe5c490a3d7f6
ilhwan.ryu <ih.ryu.dev@gmail.com>
2020-02-04 19:47:55 +0900

    wip

---
+++ test/Stack.test.js
@@ -0,0 +1,15 @@
+import { Stack } from '../src/Stack'
+
+describe('Stack', () => {
+  let stack
+  beforeEach(() => {
+    stack = new Stack()
+  })
+  describe('crud', ()=>{
+    it('push', ()=>{
+
+    })
+    it('pop')
+    it('peek')
+  })
+})
