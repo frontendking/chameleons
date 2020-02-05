40b5a2d96e68a5c6a3b2008e8c7ed39c07e2c4df
ryuilhwan <ih.ryu.dev@gmail.com>
2020-02-05 14:27:56 +0900

    wip

--- src/Stack.js
+++ src/Stack.js
@@ -1,3 +1,6 @@
 export class Stack extends Array {
-
+  push(...args){
+    super.push(...args)
+    return this
+  }
 }
--- test/Stack.test.js
+++ test/Stack.test.js
@@ -1,13 +1,14 @@
 import { Stack } from '../src/Stack'
+import { strict as assert } from 'assert'
 
 describe('Stack', () => {
   let stack
   beforeEach(() => {
-    stack = new Stack()
+    stack = new Stack(1, 2)
   })
-  describe('crud', ()=>{
-    it('push', ()=>{
-
+  describe('crud', () => {
+    it('push', () => {
+      assert.deepEqual(stack.push(3), new Stack(1, 2, 3))
     })
     it('pop')
     it('peek')
