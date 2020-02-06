26318f2c602e77d8ca18a5533ee130cfb01b5079
ryuilhwan <ih.ryu.dev@gmail.com>
2020-02-06 12:33:29 +0900

    wip

--- example/stack/example.js
+++ example/stack/example.js
@@ -5,6 +5,7 @@ const s = new Stack()
 s.push('David', 'Raymond', 'Brayan')
 console.log('length: ' + s.length)
 console.log(s.peek())
+// TODO s.pop -> s.peak,  so pop mehotd need to be refactored
 var popped = s.pop()
 console.log('The popped element is: ' + popped)
 console.log(s.peek())
