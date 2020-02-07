896d650528cdcc37821c2abf942be3e7b2d444eb
ryuilhwan <ih.ryu.dev@gmail.com>
2020-02-07 12:35:23 +0900

    implement feature of queue

--- src/Queue.js
+++ src/Queue.js
@@ -1,7 +1,21 @@
 export class Queue extends Array {
   enqueue(...args){
     args.reverse()
-    this.shift(...args)
+    this.unshift(...args)
     return this
+  }
+  dequeue(){
+    this.pop()
+    return this
+  }
+  empty(){
+    this.length = 0
+    return this
+  }
+  front(){
+    return this[0]
+  }
+  back(){
+    return this[this.length-1]
   }
 }
