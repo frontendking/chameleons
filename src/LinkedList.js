86a9ed3f12001131925b20ca5ac6d2554957e9de
ryuilhwan <ih.ryu.dev@gmail.com>
2020-02-13 10:32:50 +0900

    wip

--- src/LinkedList.js
+++ src/LinkedList.js
@@ -1,22 +1,15 @@
 export class LinkedList extends Map {
-  constructor(...args){
-    super(args.map((v,k,o)=>[v,o[k+1]]))
+  constructor (...args) {
+    super(args.map((v, k, o) => [v, o[k + 1]]))
     this.set('head', args[0])
   }
-  *[Symbol.iterator](){
-    yield 1
-    yield 2
-/*    let currNode = this.get('head')
-    while(currNode = this.get(currNode)){
+
+  * [Symbol.iterator] () {
+    let currNode = 'head'
+    while (currNode = this.get(currNode)) {
       yield currNode
-    }*/
-  }
-  test(){
-    console.log(this);
+    }
   }
 }
 
-let res = new LinkedList(1,2)
-// console.log([...res])
-res.test()
 
---
+++ test/LinkedList.test.js
@@ -0,0 +1,9 @@
+import { LinkedList } from '../src/LinkedList'
+import { strict as assert } from 'assert'
+
+describe('LinkedList', () => {
+  let llist
+  beforeEach(()=>{
+    llist = new LinkedList()
+  })
+})
