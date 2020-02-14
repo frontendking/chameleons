972d943c63e8e4537b3510ca952a955b52e02560
ilhwan.ryu <ih.ryu.dev@gmail.com>
2020-02-14 19:18:10 +0900

    wip

--- src/LinkedList.js
+++ src/LinkedList.js
@@ -1,17 +1,30 @@
 export class LinkedList extends Map {
   constructor (...args) {
-    super(args.map((v, k, o) => [v, o[k + 1]]))
-    this.set('head', args[0])
+    super(args.map((v, k, o) => [v, [o[k-1], o[k + 1]]]))
+    this.set('head', [undefined,args[0]])
+    console.log(this)
   }
 
   * [Symbol.iterator] () {
     let currNode = 'head'
     while (currNode = this.get(currNode)) {
-      yield currNode
+      yield currNode[1]
     }
   }
-  find(el){
-    return this.get(el)
+
+  insert(newItem, prevItem){
+    const prevItemInfo = this.get(prevItem)
+    this.set(prevItem, [prevItemInfo[0],newItem])
+    this.set(newItem, [prevItem, prevItemInfo[1]])
+    return this
+  }
+
+  remove(item){
+    const nextOfitem = this.get(item)
+    this.delete(item)
+    this.set(nextOfitem)
+
+    return this
   }
 }
 
--- test/LinkedList.test.js
+++ test/LinkedList.test.js
@@ -3,14 +3,22 @@ import { strict as assert } from 'assert'
 
 describe('LinkedList', () => {
   let llist
+  let item
   beforeEach(()=>{
     llist = new LinkedList()
-  })
-  it('find', ()=>{
-    llist = new LinkedList(1,2,3)
-    assert.deepEqual(llist.find(2),2)
+    item = {label:'new2', value: 2}
   })
   it('insert', ()=>{
-
+    llist = new LinkedList(1,2,3)
+    console.log(llist)
+    const newItem = item
+    const res = llist.insert(newItem, 2)
+    // assert.deepEqual([...res], [1,2,newItem,3])
   })
+/*  it('remove', ()=>{
+    llist = new LinkedList(1,2,3)
+    const res = llist.insert(item,1).remove(item)
+    assert.deepEqual([...res], [1,2,3])
+
+  })*/
 })
